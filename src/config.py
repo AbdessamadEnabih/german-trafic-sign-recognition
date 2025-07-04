5c7a3b0cedc827b5c44a07d7dc75526cd73956ac
Abdessamad Enabih <a.enabih@gmail.com>
2025-07-04 10:51:17 +0100

    Load environment variables from .env file and define data directory and Kaggle credentials

--- src/config.py
+++ src/config.py
@@ -1,2 +1,14 @@
-DATA_DIR = "data/gtsrb-german-traffic-sign"
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Define the data directory and Kaggle credentials
+DATA_DIR = os.getenv("DATA_DIR", "data/gtsrb-german-traffic-sign")
+DATASET_URL = os.getenv("DATASET_URL", "meowmeowmeowmeowmeow/gtsrb-german-traffic-sign")
+KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME")
+KAGGLE_KEY = os.getenv("KAGGLE_KEY")
+
+
 
